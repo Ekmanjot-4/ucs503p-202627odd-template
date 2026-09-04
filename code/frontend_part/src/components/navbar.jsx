7dec0b91ab9410eac92ee969519b6708b5a65a8a
Mannatpreet Singh <preetmannat415@gmail.com>
2026-09-04 21:01:20 +0530

    Added tailwind

---
+++ code/frontend_part/src/components/navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+    return (
+        <nav>
+            <div>
+                <h1>Algo Recall</h1>
+            </div>
+
+            <div>
+                <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="Profile"/>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
